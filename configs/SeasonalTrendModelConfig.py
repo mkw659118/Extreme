a7646a74942ac4f44145068ed8ecf0fb9fbb704c
mkw <1938925342@qq.com>
2025-06-08 19:16:09 +0800

    新增移动平均周期趋势性分解方法

---
+++ configs/SeasonalTrendModelConfig.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+# Author: mkw
+# Date: 2025-06-08 14:37
+# Description: SeasonalTrendModelConfig
+
+from configs.default_config import *
+from dataclasses import dataclass
+
+from configs.MainConfig import OtherConfig
+
+
+@dataclass
+class SeasonalTrendModelConfig(ExperimentConfig, BaseModelConfig, LoggerConfig, DatasetInfo, TrainingConfig, OtherConfig):
+    model: str = 'seasonal_trend_model'
+    bs: int = 32
+    rank: int = 32
+    epochs: int = 200
+    patience: int = 50
+    verbose: int = 1
+    num_layers: int = 2
+    revin: bool = True
+    d_model: int = 50
+    kernel_size: int = 25
+    individual: bool = True
