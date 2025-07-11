4e143eb134ecd7c8e77c33514e3933f8e16feb48
Yeah-le <2245005776@qq.com>
2025-07-11 20:00:50 +0800

    更改了lamda和step sh文件

--- script/run_diff_step.sh
+++ script/run_diff_step.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 pred_lens=(96 192 336 720)
-noise_steps=40
+noise_steps_arr=(10 20 30 40 50 60 100 200 1000)
 lamda=0.7
 diffusion_flags=(True)
 for len in "${pred_lens[@]}"
 do
   for diffusion in "${diffusion_flags[@]}"
   do
+    for noise_steps in "${noise_steps_arr[@]}"
+    do
       echo "Running with pred_len=$len, diffusion=$diffusion"
       python run_train.py \
         --exp_name "Transformer2Config" \
@@ -17,45 +19,6 @@ do
         --diffusion "$diffusion" \
         --noise_steps "$noise_steps" \
         --lamda "$lamda"
+    done
   done
 done
-
-# diffusion_flags=(True)
-# for len in "${pred_lens[@]}"
-# do
-#   for diffusion in "${diffusion_flags[@]}"
-#   do
-#     for noise_steps in "${noise_steps_arr[@]}"
-#     do
-#       echo "Running with pred_len=$len, diffusion=$diffusion, noise_steps=$noise_steps"
-#       python run_train.py \
-#         --exp_name "Transformer2Config" \
-#         --rounds 3 \
-#         --retrain 1 \
-#         --pred_len "$len" \
-#         --revin True \
-#         --diffusion "$diffusion" \
-#         --noise_steps "$noise_steps"
-#     done
-#   done
-# done
-
-
-# for len in "${pred_lens[@]}"
-# do
-#   for diffusion in "${diffusion_flags[@]}"
-#   do
-#     for lamda in "${lamda_arr[@]}"
-#     do
-#       echo "Running with pred_len=$len, diffusion=$diffusion,lamda=$lamda"
-#       python run_train.py \
-#         --exp_name "Transformer2Config" \
-#         --rounds 3 \
-#         --retrain 1 \
-#         --pred_len "$len" \
-#         --revin True \
-#         --diffusion "$diffusion"\
-#         --lamda "$lamda"
-#     done
-#   done
-# done
