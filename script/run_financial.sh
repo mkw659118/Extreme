fc1c8bd530a0bb3fad548bec4a04e009bb23a3de
RTX4090 <397125932@qq.com>
2025-07-15 17:52:23 +0800

    写shell文件给A100跑

--- script/run_financial.sh
+++ script/run_financial.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 # python run_cluster.py
-for i in {0..10}
+for i in {0..15}
 do
     python run_train.py --idx $i --seq_len 17 --pred_len 7
     python run_train.py --idx $i --seq_len 36 --pred_len 30
     python run_train.py --idx $i --seq_len 36 --pred_len 60
     python run_train.py --idx $i --seq_len 36 --pred_len 90
 done
-# python run_service.py
+python run_plot.py
+python run_service.py
