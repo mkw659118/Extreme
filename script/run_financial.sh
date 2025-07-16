be2bad40939d10a32d8ad979e6211f8c1bc99b7f
ZengYuXiang7 <397125932@qq.com>
2025-07-16 15:12:48 +0800

    发现bug，继续修改+

--- script/run_financial.sh
+++ script/run_financial.sh
@@ -2,19 +2,19 @@
 # python run_cluster.py
 for i in {0..0}
 do
-    python run_train.py --idx $i --seq_len 36 --pred_len 7 --topelize False
-    python run_train.py --idx $i --seq_len 36 --pred_len 30 --topelize False
-    python run_train.py --idx $i --seq_len 36 --pred_len 60 --topelize False
-    python run_train.py --idx $i --seq_len 36 --pred_len 90 --topelize False
+    python run_train.py --idx $i --seq_len 36 --pred_len 7 --Constraint False
+    python run_train.py --idx $i --seq_len 36 --pred_len 30 --Constraint False
+    python run_train.py --idx $i --seq_len 36 --pred_len 60 --Constraint False
+    python run_train.py --idx $i --seq_len 36 --pred_len 90 --Constraint False
 done
 python run_plot.py
 
 for i in {0..0}
 do
-    python run_train.py --idx $i --seq_len 36 --pred_len 7 --topelize True
-    python run_train.py --idx $i --seq_len 36 --pred_len 30 --topelize True
-    python run_train.py --idx $i --seq_len 36 --pred_len 60 --topelize True
-    python run_train.py --idx $i --seq_len 36 --pred_len 90 --topelize True
+    python run_train.py --idx $i --seq_len 36 --pred_len 7 --Constraint True
+    python run_train.py --idx $i --seq_len 36 --pred_len 30 --Constraint True
+    python run_train.py --idx $i --seq_len 36 --pred_len 60 --Constraint True
+    python run_train.py --idx $i --seq_len 36 --pred_len 90 --Constraint True
 done
 python run_plot.py
 # python run_service.py
