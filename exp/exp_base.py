7d83d37bfb46b9f0cae164f718523de61158b3aa
RTX4090 <397125932@qq.com>
2025-09-09 11:05:13 +0800

    删除无关的代码

--- exp/exp_base.py
+++ exp/exp_base.py
@@ -88,8 +88,8 @@ class BasicModel(torch.nn.Module):
         reals = torch.cat(reals, dim=0)
         preds = torch.cat(preds, dim=0)
 
-        if self.config.dataset != 'weather':
-            reals, preds = dataModule.y_scaler.inverse_transform(reals), dataModule.y_scaler.inverse_transform(preds)
+        # if self.config.dataset != 'weather':
+        reals, preds = dataModule.y_scaler.inverse_transform(reals), dataModule.y_scaler.inverse_transform(preds)
 
         if mode == 'valid':
             self.scheduler.step(val_loss)
--- script/tempCodeRunnerFile.sh
+++
@@ -1,7 +0,0 @@
-for i in {0..160}
-# do
-#     python run_train.py --idx $i --seq_len 36 --pred_len 7 --constraint False
-#     python run_train.py --idx $i --seq_len 36 --pred_len 30 --constraint False
-#     python run_train.py --idx $i --seq_len 36 --pred_len 60 --constraint False
-#     python run_train.py --idx $i --seq_len 36 --pred_len 90 --constraint False
-# done
